d662ff2a22f9b143dd53268cdb1dbf9b8c8e9ca4
216238-alt <216238@pp.balbharati.org>
2025-12-11 08:13:06 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    . # # # .
+    . . . . .
+    `)
+music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+basic.forever(function () {
+	
+})
